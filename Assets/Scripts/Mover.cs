fa716cc310d2efc594013fec16b63ab9d64ae28d
KonH <konh@yandex.ru>
2018-03-09 15:11:32 +0700

    Simple mover;

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Mover : MonoBehaviour {
+	public float MoveCoeff;
+	public float RotateCoeff;
+
+	Rigidbody _rb;
+
+	void Awake() {
+		_rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate() {
+		var moveDelta = transform.forward * Input.GetAxis("Vertical") * MoveCoeff;
+		_rb.MovePosition(transform.position + moveDelta);
+		var rotateDelta = Quaternion.AngleAxis(Input.GetAxis("Horizontal") * RotateCoeff, Vector3.up);
+		_rb.MoveRotation(transform.rotation * rotateDelta);
+	}
+}
