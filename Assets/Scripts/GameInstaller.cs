cbed890a7146788e7f1539542ac3f0e0df8269dc
KonH <konh@yandex.ru>
2018-03-09 13:52:42 +0700

    Basic UDBase setup;

---
+++ Assets/Scripts/GameInstaller.cs
@@ -0,0 +1,12 @@
+﻿using UDBase.Installers;
+using UDBase.Controllers.LogSystem;
+
+public class GameInstaller : UDBaseInstaller {
+
+	public UnityLog.Settings UnityLogSettings;
+
+	public override void InstallBindings() {
+		AddUnityLogger(UnityLogSettings);
+		AddEvents();
+	}
+}
